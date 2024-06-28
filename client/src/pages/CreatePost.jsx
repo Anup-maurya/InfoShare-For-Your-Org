69668c4280f614682fdec02e1ee69f996423c827
Anup-maurya <9anup.maurya@gmail.com>
2024-06-29 00:28:36 +0530

    update

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -103,9 +103,9 @@ export default function CreatePost() {
             }
           >
             <option value='uncategorized'>Select a category</option>
-            <option value='Notice'>Notice</option>
-            <option value='reactjs'>React.js</option>
-            <option value='Blog'>Blog</option>
+            <option value='notice'>Notice</option>
+            <option value='news'>News</option>
+            <option value='blogs'>Blogs</option>
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -131,9 +131,9 @@ export default function Search() {
               id='category'
             >
               <option value='uncategorized'>Uncategorized</option>
-              <option value='news'>News</option>
-              <option value='blogs'>Blogs</option>
               <option value='notice'>Notice</option>
+            <option value='news'>News</option>
+            <option value='blogs'>Blogs</option>
             </Select>
           </div>
           <Button type='submit' outline gradientDuoTone='purpleToPink'>
