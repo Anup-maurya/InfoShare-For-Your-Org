99364eebe32b9ebfb6ec433fcdc8776b31190c4c
Anup-maurya <9anup.maurya@gmail.com>
2024-06-28 19:58:11 +0530

    new config

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,13 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import dotenv from 'dotenv';
+dotenv.config();
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
   authDomain: "infoshare-8c065.firebaseapp.com",
   projectId: "infoshare-8c065",
   storageBucket: "infoshare-8c065.appspot.com",
