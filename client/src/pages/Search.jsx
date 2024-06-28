f1f30d7b5768021bc4a77206c5a1224acdbdb931
Anup-maurya <9anup.maurya@gmail.com>
2024-06-29 00:11:41 +0530

    category update

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -133,7 +133,7 @@ export default function Search() {
               <option value='uncategorized'>Uncategorized</option>
               <option value='reactjs'>React.js</option>
               <option value='nextjs'>Next.js</option>
-              <option value='javascript'>JavaScript</option>
+              <option value='Notice'>Notice</option>
             </Select>
           </div>
           <Button type='submit' outline gradientDuoTone='purpleToPink'>
